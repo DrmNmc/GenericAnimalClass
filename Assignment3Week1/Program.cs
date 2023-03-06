198c996b9db9ece033875899b86d95777c383c60
Dylan <mill1429@mail.nmc.edu>
2023-03-06 18:35:47 -0500

    init

--- Assignment3Week1/Program.cs
+++ Assignment3Week1/Program.cs
@@ -1,42 +1,16 @@
-﻿class Student
-{
-    public int Id { get; init; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-    public Student()
-    {
-        Id = 0;
-        FirstName = "";
-        LastName = "";
-    }
-
-    public Student(int i)
-    {
-        Id = i;
-        FirstName = "";
-        LastName = "";
-    }
-
-    public Student(int i, string first, string last)
-    {
-        Id = i;
-        FirstName = first;
-        LastName = last;
-    }
-}
+﻿public record Bookstore(int ID, string Title, string Author, double Price);
 
 class Program
 {
     static void Main(string[] args)
     {
-        Student student1 = new Student(20);
-        student1.FirstName = "John";
-        student1.LastName = "Smith";
+        Bookstore book1 = new Bookstore(1, "The Hunger Games", "Suzanne Collins", 7.99);
+        Bookstore book2 = new Bookstore(2, "The Girl on the Train", "Paula Hawkins", 10.99);
+        Bookstore book3 = new Bookstore(3, "The Nightingale", "Kristin Hannah", 11.99);
 
-        Student student2 = new Student(30, "Jane", "Smith");
 
-        Console.WriteLine($"Student 1 - ID: {student1.Id}, First Name: {student1.FirstName}, Last Name: {student1.LastName}");
-        Console.WriteLine($"Student 2 - ID: {student2.Id}, First Name: {student2.FirstName}, Last Name: {student2.LastName}");
+        Console.WriteLine($"Book 1 - ID: {book1.ID}, Title: {book1.Title}, Author: {book1.Author}, Price: {book1.Price}");
+        Console.WriteLine($"Book 2 - ID: {book2.ID}, Title: {book2.Title}, Author: {book2.Author}, Price: {book2.Price}");
+        Console.WriteLine($"Book 3 - ID: {book3.ID}, Title: {book3.Title}, Author: {book3.Author}, Price: {book3.Price}");
     }
 }
